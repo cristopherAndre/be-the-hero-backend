2b57726c0f399b5f0c759a27b657066b25c23c9b
Cristopher Andre <cristopher.andre@hotmail.com>
2020-03-30 09:57:38 -0300

    Backend

--- tests/integration/ong.spec.js
+++ tests/integration/ong.spec.js
@@ -2,7 +2,6 @@ const request = require('supertest');
 const app = require('../../src/app');
 const connection = require('../../src/database/connection');
 
-
 describe('ONG', () => {
   beforeEach(async () => {
     await connection.migrate.rollback();
@@ -11,22 +10,18 @@ describe('ONG', () => {
 
   afterAll(async () => {
     await connection.destroy();
-  })
+  });
 
   it('should be able to create a new ONG', async () => {
+    const response = await request(app).post('/ongs').send({
+      name: 'APAD2',
+      email: 'apad@gmail.com',
+      whatsapp: '559612553110',
+      city: 'Jaragua',
+      uf: 'SC',
+    });
 
-    const response = await request(app)
-      .post('/ongs')
-      .send({
-        name:"APAD2",
-        email: "apad@gmail.com",
-        whatsapp: "559612553110",
-        city: "Jaragua",
-        uf: "SC"
-      });
-    
     expect(response.body).toHaveProperty('id');
     expect(response.body.id).toHaveLength(8);
-      
   });
 });
--- tests/unit/generateUniqueId.spec.js
+++ tests/unit/generateUniqueId.spec.js
@@ -4,5 +4,5 @@ describe('Generate Unique ID', () => {
   it('should generate an unique ID', () => {
     const id = generateUniqueId();
     expect(id).toHaveLength(8);
-  })
-})
+  });
+});
